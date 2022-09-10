f0c1d0f94239d89d0ed4e8d21238f998c9591372
凱恩Kane <50613gary@gmail.com>
2022-09-11 00:27:05 +0800

    better color

--- pages/_app.js
+++ pages/_app.js
@@ -17,18 +17,19 @@ function MyApp({Component, pageProps}) {
 				<meta name="application-name" content="Yeecord 小工具" />
 				<meta name="apple-mobile-web-app-capable" content="yes" />
 				<meta name="apple-mobile-web-app-status-bar-style" content="black" />
-				<meta name="apple-mobile-web-app-title" content="PWA App" />
-				<meta name="description" content="Best PWA App in the world" />
+				<meta name="apple-mobile-web-app-title" content="Yeecord 小工具" />
+				<meta name="description" content="實用的數學小工具，包含進位計算機等簡便小工具，以及乾淨的操作介面" />
 				<meta name="format-detection" content="telephone=no" />
 				<meta name="mobile-web-app-capable" content="yes" />
 				<meta name="msapplication-TileColor" content="#1A202C" />
 				<meta name="msapplication-tap-highlight" content="no" />
-				<meta name="theme-color" content="#38B2AC" />
+				<meta name="theme-color" content="#1A202C" />
 				
 				<link rel="apple-touch-icon" sizes="128x128" href="https://yeecord.com/img/logo.png" />
 				<link rel="apple-touch-icon" sizes="512x512" href="https://yeecord.com/img/logo_512x512.png" />
 				<link rel="shortcut icon" href="https://yeecord.com/img/logo.png" />
 				<link rel="manifest" href="/manifest.json" />
+				<meta name="nightmode" content="enable"/>
 			</Head>
 			<Component {...pageProps} />
 		</ChakraProvider>
