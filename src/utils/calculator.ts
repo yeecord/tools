4ef2d8835f8bc7b37d83b40adebde4fc058d7533
Gary50613 <50613gary@gmail.com>
2023-06-12 15:38:32 +0800

    finish calculator route

--- src/utils/calculator.ts
+++ src/utils/calculator.ts
@@ -1,1 +1,28 @@
-export const calculatorTypes = ["dec", "hex", "oct", "bin"];
+export const calculatorTypes = ["dec", "hex", "oct", "bin"] as const;
+
+export interface CalculatorConfig {
+	title: string;
+	base: number;
+}
+
+export const calculatorConfig: Record<
+	(typeof calculatorTypes)[number],
+	CalculatorConfig
+> = {
+	dec: {
+		title: "十進位",
+		base: 10,
+	},
+	hex: {
+		title: "十六進位",
+		base: 16,
+	},
+	oct: {
+		title: "八進位",
+		base: 8,
+	},
+	bin: {
+		title: "二進位",
+		base: 2,
+	},
+};
