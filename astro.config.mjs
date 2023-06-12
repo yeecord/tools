631811bb4b642d85743b39e91018bc626a82a81a
Gary50613 <50613gary@gmail.com>
2023-06-12 11:55:01 +0800

    bump astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,26 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, sharpImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import compress from "astro-compress";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind({
-		config: {
-			applyBaseStyles: false
-		}
-	}), react()],
+	site: "https://tools.yeecord.com",
+	integrations: [
+		tailwind({
+			config: {
+				applyBaseStyles: false,
+			},
+		}),
+		react(),
+		compress(),
+		sitemap(),
+	],
+	experimental: {
+		assets: true,
+	},
+	image: {
+		service: sharpImageService(),
+	},
 });
