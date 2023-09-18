b7eef4272f7da710bfc5369c253ad470bc4b7301
Gary50613 <50613gary@gmail.com>
2023-09-18 14:19:43 +0800

    try register

---
+++ src/pwa.ts
@@ -0,0 +1,12 @@
+// @ts-ignore
+import { registerSW } from "virtual:pwa-register";
+
+registerSW({
+  immediate: true,
+  onRegisteredSW(swScriptUrl: string) {
+    console.log('SW registered: ', swScriptUrl)
+  },
+  onOfflineReady() {
+    console.log('PWA application ready to work offline')
+  },
+})
