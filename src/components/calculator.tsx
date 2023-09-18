b961dcd1228b894c85dddf545548c2e01ecb9b95
Gary50613 <50613gary@gmail.com>
2023-09-18 11:42:45 +0800

    add select

--- src/components/calculator.tsx
+++ src/components/calculator.tsx
@@ -3,18 +3,30 @@ import type { CalculatorConfig } from "@/utils/calculator";
 import { useCalculator } from "@/hooks/useCalculator";
 import { useId } from "react";
 import { cn } from "@/utils/cn";
+import { CalculatorSelect } from "@/components/calculator-select.tsx";
+import { calculatorTypes } from "@/utils/calculator";
 
 export const Calculator: FC<{
   from: CalculatorConfig;
+  fromId: (typeof calculatorTypes)[number];
   to: CalculatorConfig;
-}> = ({ from, to }) => {
+  toId: (typeof calculatorTypes)[number];
+}> = ({ from, to, fromId, toId }) => {
   const { fromValue, toValue, setFromValue, setToValue, fromValid, toValid } =
     useCalculator(from, to);
 
   return (
     <div className="space-y-8 mt-8">
       <div className="space-y-4">
-        <h4 className="text-xl font-semibold tracking-tight">從</h4>
+        <div className="flex gap-2 items-center">
+          <h4 className="text-xl font-semibold tracking-tight">從</h4>
+          <CalculatorSelect
+            defaultValue={fromId}
+            onValueChange={(value) =>
+              value !== toId && (location.href = `/calculator/${value}/${toId}`)
+            }
+          />
+        </div>
         <Item
           {...from}
           set={setFromValue}
@@ -23,7 +35,18 @@ export const Calculator: FC<{
         />
       </div>
       <div className="space-y-4">
-        <h4 className="text-xl font-semibold tracking-tight">轉為</h4>
+        <div className="flex gap-2 items-center">
+          <h4 className="text-xl font-semibold tracking-tight whitespace-nowrap">
+            轉為
+          </h4>
+          <CalculatorSelect
+            defaultValue={toId}
+            onValueChange={(value) =>
+              value !== fromId &&
+              (location.href = `/calculator/${fromId}/${value}`)
+            }
+          />
+        </div>
         <Item {...to} set={setToValue} valid={toValid} value={toValue} />
       </div>
     </div>
@@ -48,7 +71,7 @@ const Item: FC<
         value={value}
         className={cn(
           "font-medium text-base border font-mono max-w-xl",
-          !valid && "focus-visible:ring-0 border-red-700"
+          !valid && "focus-visible:ring-0 border-red-700",
         )}
         onChange={(event) => set(event.target.value.trim())}
       />
