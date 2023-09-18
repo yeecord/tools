73c1ff445bca445651e239907f719b4e353483d9
Gary50613 <50613gary@gmail.com>
2023-09-18 14:21:25 +0800

    without logging

--- src/pwa.ts
+++ src/pwa.ts
@@ -3,10 +3,4 @@ import { registerSW } from "virtual:pwa-register";
 
 registerSW({
   immediate: true,
-  onRegisteredSW(swScriptUrl: string) {
-    console.log('SW registered: ', swScriptUrl)
-  },
-  onOfflineReady() {
-    console.log('PWA application ready to work offline')
-  },
 })
