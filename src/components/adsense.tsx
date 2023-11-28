0ad73ff4dd6f1602058eefc4e7c97382e9a26725
Gary50613 <50613gary@gmail.com>
2023-11-28 14:14:30 +0800

    fixed space

--- src/components/adsense.tsx
+++ src/components/adsense.tsx
@@ -1,12 +1,16 @@
 export const Adsense = () => (
-  <>
+  <div className="h-40 w-full my-4">
     <ins
       className="adsbygoogle block text-center"
       data-ad-layout="in-article"
       data-ad-format="fluid"
       data-ad-client="ca-pub-1801171681307308"
       data-ad-slot="4950823280"
-    ></ins>
-    <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
-  </>
+    />
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `(adsbygoogle = window.adsbygoogle || []).push({});`,
+      }}
+    />
+  </div>
 );
