f160eebb676ae1b32356edac24cdf59c5c29ce31
Gary50613 <50613gary@gmail.com>
2023-12-02 08:23:45 +0800

    fix title

--- src/utils/title.ts
+++ src/utils/title.ts
@@ -1,5 +1,5 @@
 import { calculatorConfig, type CalculatorType } from "@/utils/calculator.ts";
 
 export function createTitle(fromId: CalculatorType, toId: CalculatorType) {
-  return `即時${calculatorConfig[fromId].title}進制轉${calculatorConfig[toId].title}進制轉換器`;
+  return `即時${calculatorConfig[fromId].title}進制轉${calculatorConfig[toId].title}進位轉換器`;
 }
