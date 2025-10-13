5498cbf02eaff974367ffc31d6cc1e1cecce9c75
Kane Wang <kane@yeecord.com>
2025-10-13 21:31:58 +0800

    make address json file static

--- src/pages/address-to-english.json.ts
+++ src/pages/address-to-english.json.ts
@@ -1,14 +1,7 @@
 import { getAddressToEnglishJson } from "~/utils/get-address-data-json";
 
-export const prerender = false;
+export const prerender = true;
 
 export async function GET() {
-  const result = await getAddressToEnglishJson();
-
-  return Response.json(result, {
-    headers: {
-      "content-type": "application/json",
-      "cache-control": "public, stale-while-revalidate=3600, max-age=3600",
-    },
-  });
+  return Response.json(await getAddressToEnglishJson());
 }
--- src/utils/get-address-data-json.ts
+++ src/utils/get-address-data-json.ts
@@ -31,6 +31,8 @@ async function getCounty() {
   const countyParsed = new XMLParser({
     // keep it all string
     parseTagValue: false,
+    ignoreAttributes: true,
+    removeNSPrefix: true,
   }).parse(countyText) as {
     dataroot: {
       County_h_10906: Record<string, string>[];
@@ -53,12 +55,14 @@ async function getVillages() {
   return Papa.parse<[string, string]>(villageText, {
     header: false,
     skipEmptyLines: true,
+    worker: false,
+    delimiter: ",",
   });
 }
 
 async function getRoads() {
   const roadsResponse = await fetch(
-    "https://www.post.gov.tw/post/download/%E4%B8%AD%E8%8B%B1%E6%96%87%E8%A1%97%E8%B7%AF%E5%90%8D%E7%A8%B1%E5%B0%8D%E7%85%A7%E6%AA%941130401.TXT",
+    "https://www.post.gov.tw/post/download/%E4%B8%AD%E8%8B%B1%E6%96%87%E8%A1%97%E8%B7%AF%E5%90%8D%E7%A8%B1%E5%B0%8D%E7%85%A7%E6%AA%94.TXT",
   );
 
   // convert big5 to utf-8
@@ -69,9 +73,14 @@ async function getRoads() {
   return Papa.parse<[string, string]>(roadsDecoded, {
     header: false,
     skipEmptyLines: true,
+    worker: false,
+    delimiter: ",",
   });
 }
 
 function fixTextFile(text: string) {
-  return text.replace(//g, "").replace(/台/g, "臺").replace(/\r\n/g, "\n");
+  return text
+    .replaceAll("", "")
+    .replaceAll("台", "臺")
+    .replaceAll("\r\n", "\n");
 }
