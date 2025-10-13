d4a0535f16d871e1dcc3f6d4d7280c645f3f7b59
Kane Wang <kane@yeecord.com>
2025-10-13 20:53:02 +0800

    lint

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,9 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import compress from "@playform/compress";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, passthroughImageService } from "astro/config";
 import robotsTxt from "astro-robots-txt";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
